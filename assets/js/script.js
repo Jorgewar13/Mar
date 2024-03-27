944391ee7b79840b85a7ffdfe9f4a97006ebef89
Jorgewar13 <Jorgewar13@gmail.com>
2024-03-27 14:14:16 +0100

    vr añadido

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,87 @@
-function actualizarPuntaje() {
-  const scoreText = document.getElementById('scoreText');
-  let score = parseInt(scoreText.getAttribute('value').split(':')[1].trim()); // Obtener el puntaje actual
-  score++;
-  scoreText.setAttribute('value', `Score: ${score}`);
+AFRAME.registerComponent('log', {
   
-  // Verificar si todos los elementos objetivo han sido eliminados
-  if (document.querySelectorAll('.target').length === 0) {
-    console.log('you win');
+  init: function () {
+    var debugtxt = document.querySelector('a-text');
+    debugtxt.setAttribute('value', 'goodnite');
   }
-}
+});
 
-const shoot = () => {
-  const bullet = document.createElement("a-sphere");
-  let pos = myCamera.getAttribute("position");
-  bullet.setAttribute("position", pos);
-  bullet.setAttribute("velocity", getDirection(myCamera, 30));
-  bullet.setAttribute("dynamic-body", true);
-  bullet.setAttribute("radius", 0.5);
-  bullet.setAttribute("src", "https://i.imgur.com/H8e3Vnu.png ");
-  myScene.appendChild(bullet);
-  bullet.addEventListener('collide', shootCollided); // cuando hay una collide(evento) choque entre objetos se activa la funcion shootCollided(manejador de ebventos)
-};
-
-const shootCollided = event => {
-  if (event.detail.body.id === 'floor') {
-    console.log('hit the floor');
-    event.detail.el.removeEventListener('collide', shootCollided);
-    myScene.removeChild(event.detail.target.el);
-  } else if (event.detail.body.el.className === 'target') {
-    console.log('hit the target');
-    event.detail.target.el.removeEventListener('collide', shootCollided);
-    myScene.removeChild(event.detail.target.el);
-    myScene.removeChild(event.detail.body.el);
+AFRAME.registerComponent('thumbstick-logging',{
+  init: function () {
+    this.el.addEventListener('thumbstickmoved', this.logThumbstick.bind(this));
+  },
+  logThumbstick: function (evt) {
+    var debugtxt = document.querySelector('a-text');
+    var cameraRig = document.querySelector('#cameraRig');
     
-    // Llama a la función para actualizar el puntaje
-    actualizarPuntaje();
+    if (evt.detail.y > 0.95) { 
+      debugtxt.setAttribute("value","DOWN"); 
+      cameraRig.object3D.translateZ(0.05);
+    }
+    if (evt.detail.y < -0.95) { 
+      debugtxt.setAttribute("value","UP");
+      cameraRig.object3D.translateZ(-0.05);
+    }    
+    if (evt.detail.x < -0.95) { 
+      debugtxt.setAttribute("value","LEFT");
+      cameraRig.object3D.rotateY(THREE.Math.degToRad(5));
+    }
+    if (evt.detail.x > 0.95) { 
+      debugtxt.setAttribute("value","RIGHT");
+      cameraRig.object3D.rotateY(THREE.Math.degToRad(-5));
+    }
   }
-};
+});
 
-document.onkeydown = event => {
-  if (event.which == 32) {
-    shoot();
+AFRAME.registerComponent('collider-check', {
+  dependencies: ['raycaster'],
+
+  init: function () {
+   
+    console.log('ok collider check');
+    var debugtxt = document.querySelector('a-text');
+    
+    this.el.addEventListener('raycaster-intersection', function (e) {
+     //-- get selected object
+      this.selectedObj = e.detail.els[0];
+      debugtxt.setAttribute('value', 'Player hit something!');
+    });
+    
+    //-- grip button pressed
+    this.el.addEventListener('gripdown', function (e) {
+        this.grip = true;
+        debugtxt.setAttribute('value', 'Grip button pressed');
+    });
+    
+    //-- grip button released
+    this.el.addEventListener('gripup', function (e) {
+        this.grip = false;
+        debugtxt.setAttribute('value', 'Grip button released');
+    });
+  },
+  
+  tick: function(){
+    if(!this.el.selectedObj) return;
+    if(!this.el.grip) return;
+    
+    
+    var raycast = this.el.getAttribute("raycaster").direction;
+    
+    var pos = new THREE.Vector3(raycast.x, raycast.y, raycast.z);
+    pos.normalize();
+    
+    //-- final destination of object will be 2m in front of ray
+    pos.multiplyScalar(2);
+              
+    //-- convert to world coordinate
+    this.el.object3D.localToWorld(pos);
+    
+    //Move selected object to follow the tip of raycaster.
+    this.el.selectedObj.object3D.position.set(pos.x, pos.y, pos.z);
+    
+    
+    if (this.el.selectedObj.components["dynamic-body"]) {
+      this.el.selectedObj.components["dynamic-body"].syncToPhysics();
+    }
   }
-};
+});
