8cfe4fdbce6f7ba0b108bee1bdaec59cc86e6e30
anhi1 <anhiemolina@gmail.com>
2024-04-03 10:13:01 +0200

    test

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'svzywv',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
